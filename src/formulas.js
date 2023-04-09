32078114d1f24256bf72b4f7f7b85a7f30652521
MUELLK <kilian.mueller90@gmail.com>
2023-04-10 00:58:52 +0200

    Update

--- src/formulas.js
+++ src/formulas.js
@@ -127,22 +127,13 @@ function sumifs() {
 
                 // If one is date, convert both to dates
                 const dateRegex = /^\d{1,2}\/\d{1,2}\/\d{4}$/
-                /*function isUnixTimestamp(timestamp) {
-                    let date = formatter.format(new Date(timestamp).getTime())
-                    if(date!= "1/1/1970" && timestamp != "<0" && timestamp != ">0"){
-                        return dateRegex.test(date)
-                    } else {
-                        return false
-                    }
-                }*/
-                //let valueIsUnixTimestamp = isUnixTimestamp(valueToTest);
+
                 let valueIsDate = dateRegex.test(valueToTest);
-               // let criteriaIsUnixTimestamp = isUnixTimestamp(criteria);
                 let criteriaIsDate = dateRegex.test(criteria);
-                let dateCase;
 
                 if(valueIsDate || criteriaIsDate){
-                   /* try {
+                   /*                 let dateCase;
+                    try {
                         if (valueIsUnixTimestamp) {
                             console.log(typeof valueToTest);
                             if (criteriaIsUnixTimestamp) {
@@ -339,9 +330,9 @@ function edate(start_date, months) {
     } else {
         months = parseInt(months, 10)
         start_date.setMonth(start_date.getMonth() + months)
-        console.log("edate 3: "+start_date+" - "+months);
+        //console.log("edate 3: "+start_date+" - "+months);
         start_date = formatter.format(start_date);
-        console.log("edate 4: "+start_date+" - "+months);
+        //console.log("edate 4: "+start_date+" - "+months);
 
         return start_date
     }
