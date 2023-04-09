110244e096f9aea03dd99087d49e1ced716c071e
MUELLK <kilian.mueller90@gmail.com>
2023-04-09 16:35:23 +0200

    Update

--- src/Exp.js
+++ src/Exp.js
@@ -191,8 +191,8 @@ module.exports = function Exp(formula) {
             return a < b;
         });
         exec('>', args, function(a, b) {
-            console.log(args);
-            console.log("a: " + a + " b: " + b);
+           // console.log(args);
+          //  console.log("a: " + a + " b: " + b);
             return a > b;
         });
         exec('>=', args, function(a, b) {
