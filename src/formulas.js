9f88ca3dc9d3fc9c650b6f342b145e41122343ac
MUELLK <kilian.mueller90@gmail.com>
2023-04-09 17:17:19 +0200

    Update

--- src/formulas.js
+++ src/formulas.js
@@ -842,9 +842,9 @@ function concatenate() {
 }
 
 function sum() {
-    //if(arguments.length == 2 && arguments[0].length == 5){
+    if(arguments[0].length == 241){
        console.log(arguments);
-    //}
+    }
 
     var r = 0;
     for (var i = arguments.length; i--;) {
